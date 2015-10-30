8b2ad55bbfb1de31c7bb8f2f89cdfd0e06bfc30f
David Przybilla <dav.alejandro@gmail.com>
2015-10-30 23:44:15 +0000

    adding example

---
+++ example.py
@@ -0,0 +1,49 @@
+import json
+import logging
+
+
+from lxml.cssselect import CSSSelector
+
+from tarantula.scraper import ScraperTask, scrape_tasks, scrape
+from tarantula.parser import parser
+from tarantula.parsers.html import HtmlParser
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%m-%d %H:%M')
+
+logger = logging.getLogger("tarantula")
+
+
+class SampleHTMLParser(HtmlParser):
+
+  def extract(self, parsed_data):
+    number_of_anchors = len(CSSSelector("a")(parsed_data))
+    number_of_paragraphs =  len(CSSSelector("p")(parsed_data))
+    data = { "anchors": number_of_anchors, "paragraphs": number_of_paragraphs}
+    return json.dumps(data)
+
+
+scrape_data_folder = "/Users/dav009/source/tarantula/raw_data/"
+extracted_data_folder =  "/Users/dav009/source/tarantula/extracted_data/"
+
+
+
+#tasks = []
+#for i in range(0,10000):
+#    tasks.append(ScraperTask(url="http://google.com",
+#                             output_folder="/Users/dav009/source/tarantula/data/"))
+#scrape_tasks(tasks)
+
+params = {
+     "objeto" : ["10000000", "11000000", "12000000", "15000000", "13000000", "14000000", "27000000", "20000000", "21000000"],
+     "paginaObjetivo": ["1"],
+     "cuantias": ["1", "2", "3"]
+}
+url = "https://www.contratos.gov.co/consultas/resultadosConsulta.do?&ctl00$ContentPlaceHolder1$hidIDProducto=-1&ctl00$ContentPlaceHolder1$hidRedir=&departamento=&ctl00$ContentPlaceHolder1$hidNombreDemandante=-1&ctl00$ContentPlaceHolder1$hidNombreProducto=-1&fechaInicial=&ctl00$ContentPlaceHolder1$hidIdEmpresaC=0&ctl00$ContentPlaceHolder1$hidIdOrgV=-1&ctl00$ContentPlaceHolder1$hidIDProductoNoIngresado=-1&ctl00$ContentPlaceHolder1$hidRangoMaximoFecha=&fechaFinal=&desdeFomulario=true&ctl00$ContentPlaceHolder1$hidIdOrgC=-1&ctl00$ContentPlaceHolder1$hidIDRubro=-1&tipoProceso=&registrosXPagina=10&numeroProceso=&municipio=0&estado=0&ctl00$ContentPlaceHolder1$hidNombreProveedor=-1&ctl00$ContentPlaceHolder1$hidIdEmpresaVenta=-1"
+
+
+print("scraping...")
+scrape(url, output_folder=scrape_data_folder, params_and_values=params)
+print("parsing..")
+parser(input_folder=scrape_data_folder, output_folder=extracted_data_folder, parser=SampleHTMLParser())
