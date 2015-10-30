c22dece5813a3bd5512b6e3ec4fda446e0685835
David Przybilla <dav.alejandro@gmail.com>
2015-10-30 23:43:34 +0000

    fixing tests

--- tests/test_resolver.py
+++ tests/test_resolver.py
@@ -6,12 +6,11 @@ import pytest
 
 class TestUtils:
     def test_make_request(self):
-        task = ScraperTask("http://www.google.com", "get", {} )
+        task = ScraperTask("http://www.google.com", "output_folder", method="get", params={}, headers={} )
         html_text = resolver.make_request(task)
         assert("google" in html_text)
 
     def test_failing_request(self, monkeypatch):
-        task = ScraperTask("http://www.blablanonexistangasdadasd.com", "get", {} )
-        monkeypatch.setattr(resolver.requests, 'get', mockreturn)
+        task = ScraperTask("http://www.blablanonexistangasdadasd.com", "output_folder", method="get", params={}, headers={} )
         with pytest.raises(Exception):
             resolver.make_request(task)
